25d3abb509d2f21a6247adf0f1e55c3acc245b2b
Nikita <nikitashrestha345@gmail.com>
2024-05-09 12:14:30 +0545

    reduce, reuse, recycle screens

--- lib/screens/Splash/screens/splashscreen1.dart
+++ lib/screens/Splash/screens/splashscreen1.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/route_manager.dart';
+import 'package:secondlife/screens/Splash/screens/splashscreen2.dart';
 
 class ReduceScreen extends StatefulWidget {
   const ReduceScreen({super.key});
@@ -12,25 +14,53 @@ class _ReduceScreenState extends State<ReduceScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Reduce"),
-            Text(
-                "Lessen the amount of waste produced by using less of the Earth's natural resources."),
-            Container(
-                width: 250,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(30),
-                  color: Color.fromARGB(255, 10, 150, 71),
-                ),
-                child: TextButton(
-                    onPressed: () {},
-                    child: Text(
-                      "Continue",
-                      style: TextStyle(color: Colors.white),
-                    )))
-          ],
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Reduce",
+                style: TextStyle(
+                    color: Color.fromARGB(255, 116, 117, 116),
+                    fontSize: 45,
+                    fontWeight: FontWeight.w500),
+              ),
+              SizedBox(height: 10),
+              Text(
+                "Lessen the amount of waste produced by",
+                style: TextStyle(
+                    color: Color.fromARGB(238, 134, 138, 135), fontSize: 16),
+              ),
+              Text(
+                "consuming fewer products, choosing items with",
+                style: TextStyle(
+                    color: Color.fromARGB(238, 134, 138, 135), fontSize: 16),
+              ),
+              Text(
+                "minimal packaging, and being mindful",
+                style: TextStyle(
+                    color: Color.fromARGB(238, 134, 138, 135), fontSize: 16),
+              ),
+              SizedBox(height: 20),
+              Container(
+                  height: 55,
+                  width: 250,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: Color.fromARGB(255, 10, 150, 71),
+                  ),
+                  child: TextButton(
+                      onPressed: () {
+                        Get.to(
+                          ReuseScreen(),
+                        );
+                      },
+                      child: Text(
+                        "Continue",
+                        style: TextStyle(color: Colors.white, fontSize: 18),
+                      )))
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/screens/Splash/screens/splashscreen3.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class RecycleScreen extends StatefulWidget {
+  const RecycleScreen({super.key});
+
+  @override
+  State<RecycleScreen> createState() => _RecycleScreenState();
+}
+
+class _RecycleScreenState extends State<RecycleScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Recycle",
+                style: TextStyle(
+                    color: Color.fromARGB(255, 116, 117, 116),
+                    fontSize: 45,
+                    fontWeight: FontWeight.w500),
+              ),
+              SizedBox(height: 10),
+              Text(
+                "Converting waste materials into new products",
+                style: TextStyle(
+                    color: Color.fromARGB(238, 134, 138, 135), fontSize: 16),
+              ),
+              Text(
+                "collecting and processing materials like paper,",
+                style: TextStyle(
+                    color: Color.fromARGB(238, 134, 138, 135), fontSize: 16),
+              ),
+              Text(
+                "glass, metal, and plastic to create new items",
+                style: TextStyle(
+                    color: Color.fromARGB(238, 134, 138, 135), fontSize: 16),
+              ),
+              SizedBox(height: 20),
+              Container(
+                  height: 55,
+                  width: 250,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: Color.fromARGB(255, 10, 150, 71),
+                  ),
+                  child: TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        "Continue",
+                        style: TextStyle(color: Colors.white, fontSize: 18),
+                      )))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
