852766c0ea1a3c9f80f0e9fc1c43b40dff473f01
Codewire-github <ishanawal68@gmail.com>
2024-05-09 17:48:30 +0545

    User type UI updated

--- lib/screens/Signup/screens/user_type_screen.dart
+++ lib/screens/Signup/screens/user_type_screen.dart
@@ -3,10 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:secondlife/common/back_button.dart';
+import 'package:secondlife/common/customButtons.dart';
 import 'package:secondlife/screens/Signup/screens/namescreen.dart';
 
 class UserTypeScreen extends StatefulWidget {
-  const UserTypeScreen({super.key});
+  const UserTypeScreen({Key? key}) : super(key: key);
 
   @override
   State<UserTypeScreen> createState() => _UserTypeScreenState();
@@ -14,84 +15,123 @@ class UserTypeScreen extends StatefulWidget {
 
 class _UserTypeScreenState extends State<UserTypeScreen> {
   int selectedRole = 1;
+
   @override
   Widget build(BuildContext context) {
     double screenHeight = MediaQuery.of(context).size.height;
     double screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
+      body: Stack(
         children: [
-          Row(
+          Column(
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
-              CustomBackButton(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  CustomBackButton(),
+                ],
+              ),
+              Text(
+                "Please choose your role",
+                style: TextStyle(fontSize: 28, fontWeight: FontWeight.w700),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              GestureDetector(
+                onTap: () => setState(() => selectedRole = 1),
+                child: AnimatedContainer(
+                  duration: Duration(milliseconds: 300),
+                  height: 250,
+                  width: 250,
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 235, 255, 236),
+                    borderRadius: BorderRadius.circular(selectedRole == 1
+                        ? 200
+                        : 12), // Rounded rectangle when not selected
+                  ),
+                  child: Stack(
+                    children: [
+                      Positioned.fill(
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(
+                              selectedRole == 1 ? 200 : 12),
+                          child: Image.asset('assets/img/signupscreen/user.jpg',
+                              fit: BoxFit.cover),
+                        ),
+                      ),
+                      if (selectedRole ==
+                          1) // Show checkmark icon when selected
+                        Align(
+                          alignment: Alignment.bottomRight,
+                          child: Icon(Icons.check_circle,
+                              color: Colors.green, size: 40),
+                        ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              Text(
+                "User",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+              ),
+              SizedBox(height: 15),
+              GestureDetector(
+                onTap: () => setState(() => selectedRole = 2),
+                child: AnimatedContainer(
+                  duration: Duration(milliseconds: 300),
+                  height: 250,
+                  width: 250,
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 235, 255, 236),
+                    borderRadius: BorderRadius.circular(selectedRole == 2
+                        ? 200
+                        : 12), // Rounded rectangle when not selected
+                  ),
+                  child: Stack(
+                    children: [
+                      Positioned.fill(
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(
+                              selectedRole == 2 ? 200 : 12),
+                          child: Image.asset(
+                              'assets/img/signupscreen/recycler.jpg',
+                              fit: BoxFit.cover),
+                        ),
+                      ),
+                      if (selectedRole ==
+                          2) // Show checkmark icon when selected
+                        Align(
+                          alignment: Alignment.bottomRight,
+                          child: Icon(Icons.check_circle,
+                              color: Colors.green, size: 40),
+                        ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              Text(
+                "Recycling Plant / \n Non-profit Organization",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 20),
             ],
           ),
-          Text(
-            "Please choose your role",
-            style: TextStyle(fontSize: 28, fontWeight: FontWeight.w700),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          GestureDetector(
-            onTap: () => {
-              setState(() {
-                selectedRole = 1;
-              })
-            },
-            child: AnimatedContainer(
-                duration: Duration(milliseconds: 300),
-                height: 250,
-                width: 250,
-                decoration: BoxDecoration(
-                    color: Colors.red[50],
-                    borderRadius:
-                        BorderRadius.circular(selectedRole == 1 ? 200 : 0))),
-          ),
-          Text(
-            "User",
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
-          ),
-          SizedBox(height: 15),
-          GestureDetector(
-            onTap: () => {
-              setState(() {
-                selectedRole = 2;
-              })
-            },
-            child: AnimatedContainer(
-              duration: Duration(milliseconds: 300),
-              height: 250,
-              width: 250,
-              decoration: BoxDecoration(
-                  color: Colors.red[50],
-                  borderRadius: selectedRole == 2
-                      ? BorderRadius.circular(200)
-                      : BorderRadius.circular(0)),
-            ),
-          ),
-          Text(
-            "Recycler",
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
-          ),
-          SizedBox(height: 20),
-          Container(
-              height: 55,
-              width: 250,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(22.5),
-                color: Color.fromARGB(255, 10, 150, 71),
-              ),
-              child: TextButton(
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 35.0),
+              child: CustomLargeButton(
+                  label: "Continue",
                   onPressed: () {
                     Get.to(() => NameScreen());
-                  },
-                  child: Text(
-                    "Continue",
-                    style: TextStyle(color: Colors.white, fontSize: 18),
-                  )))
+                  }),
+            ),
+          )
         ],
       ),
     );
